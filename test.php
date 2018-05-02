7c4432c56e68957fd9efa83f5a4ac494b7e464fd
Fritz Fevrier <fev17001@byui.edu>
2018-05-02 00:45:30 -0600

    added comments

--- test.php
+++ test.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
-<title></title>
+  
 <body>
 
 <h1>This is a php page</h1>
 
 <?php 
-for ($i = 0; $i <= 10; $i++) {
-    echo "<div id=\"div$i\">$i</div>\n"
-?>
 
+for ($i = 0; $i <= 10; $i++) { 
+    echo "<div id=\"div$i\">$i</div>\n";
+ 
+} 
+?>
 </body>
 </html>
