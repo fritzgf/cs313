027bb6931e80df1ab48f8fcefaabe1e8e8289fc7
Fritz Fevrier <fev17001@byui.edu>
2018-05-05 15:17:46 -0600

    new messages

--- web/view/home.php
+++ web/view/home.php
@@ -29,21 +29,22 @@
 
         <main>
 
-            <h1> This is a little bit about my home country. </h1>
+            <h2> This is a little bit about my home country. </h2>
             
-            <link rel="canonical" href="http://www.citadellelaferriere.com/" /><meta name="google-site-verification" content="APpBmTY9TIKEMYivYDCvE-2SJDnqeNpSUVNDT1uOEHI" /><link 
-            <figcaption id="picture-name"> Citadelle Laferrière </figcaption>
-            <div> 
+           <img src="./..images/citadelle.jpg">
+            <figcaption id="picture-name"> Citadelle Laferrière, Haiti </figcaption>
+            <div class="description"> 
             <p>The Citadelle Laferrière is a mountaintop fortress, located
-                                    on the northern coast of Haiti - on the top of mountain Bonnet a L&rsquo;Eveque.</p>
+     on the northern coast of Haiti - on the top of mountain Bonnet a L&rsquo;Eveque.</p>
 
-                                    <p>Depicted on local currency, stamps and postcards, this
-                                    amazing structure has become the symbol of Haiti&rsquo;s power and independence. It
-                                    was built in the beginning of the 19th century by one of the leaders of Haiti&rsquo;s
-                                    slave revolution.</p>
+          <p>Depicted on local currency, stamps and postcards, this
+            amazing structure has become the symbol of Haiti&rsquo;s power and independence. It
+            was built in the beginning of the 19th century by one of the leaders of Haiti&rsquo;s
+             slave revolution.</p>
 
-                                    <p>The Citadelle Laferrière is also known simply as the
-                                    Citadelle or as Citadelle Henri Christophe in the honor of its creator.</p>
+         <p>The Citadelle Laferrière is also known simply as the
+             Citadelle or as Citadelle Henri Christophe in the honor of its creator.</p>
+          <p> Visit this link <a href="http://www.citadellelaferriere.com">Citadelle Laferrière </a> to learn more </p> 
 </div>
             
         </main>
