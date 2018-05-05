e7e43598bbbe1ec8bcac5bfcf55857a4f78c9342
Fritz Fevrier <fev17001@byui.edu>
2018-05-05 12:36:06 -0600

    add new files

---
+++ web/view/assignment.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html  lang="en">
+
+<head>
+    <!-- head describes the page to the browser-->
+
+    <meta charset="utf-8">
+    <title> Home | fritzgf.github.io</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous">
+    </script>
+
+</head>
+
+<body>
+<!doctype html>
+<html  lang="en">
+
+<head>
+   
+
+    <meta charset="utf-8">
+    <title> Directory| assignments</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous">
+    </script>
+
+</head>
+
+<body>
+
+
+    <!--body holds the content of the page-->
+    <div id=" wrapper">
+
+
+        <header>
+
+        </header>
+        <!-- links to my future assignments-->
+        <nav>
+        <?php
+        require("common/navigation.php");
+        ?>
+        </nav>
+
+        <main>
+
+            <h1> Fritz Fevrier </h1>
+        </main>
+      
+        <footer>
+        <?php
+        require("common/footer.php");
+        ?>
+        </footer>
+    </div>
+</body>
+
+</html>
+
+
+
+ 
+    <div id=" wrapper">
+
+
+        <header>
+
+        </header>
+        
+        <nav>
+           
+        </nav>
+
+        <main>
+
+            <h1> Fritz Fevrier </h1>
+        </main>
+      
+        <footer>
+           
+        </footer>
+    </div>
+</body>
+
+</html>
+
---
+++ web/view/home.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html  lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <title> Home | </title>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous">
+    </script>
+
+</head>
+
+<body>
+
+<h1>Welcome to My Repository </h1>
+
+    <div id=" wrapper">
+
+
+        <header>
+
+        </header>
+       
+        <nav>
+            <?php
+        require("common/navigation.php");
+        ?>
+        </nav>
+
+        <main>
+
+            <h1> Fritz Fevrier </h1>
+        </main>
+      
+        <footer>
+        <?php
+        require("common/footer.php");
+        ?>
+        </footer>
+    </div>
+</body>
+
+</html>
+
