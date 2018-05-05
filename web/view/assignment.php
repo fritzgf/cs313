cd29eefb84a924d4e39574695f788fd107ac0c73
Fritz Fevrier <fev17001@byui.edu>
2018-05-05 13:59:17 -0600

    add new changes

--- web/view/assignment.php
+++ web/view/assignment.php
@@ -2,20 +2,6 @@
 <html  lang="en">
 
 <head>
-    
-
-    <meta charset="utf-8">
-    <title> Home | fritzgf.github.io</title>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous">
-    </script>
-
-</head>
-
-<body>
-<!doctype html>
-<html  lang="en">
-
-<head>
     <meta charset="utf-8">
     <title> Directory| assignments</title>
     <link rel="stylesheet" type="text/css" href="./../css/stylesheet.css" media="screen">
@@ -31,16 +17,16 @@
         </header>
         <!-- links to my future assignments-->
         <nav>
-        <?php
-        require("./../common/navigation.php");
-        ?>
+        
         </nav>
 
         <main>
-
-            <h1> Fritz Fevrier </h1>
+        <?php
+        require("./../common/navigation.php");
+        ?>
         </main>
       
+
         <footer>
         <?php
     
@@ -55,27 +41,4 @@
 
 
  
-    <div id=" wrapper">
-
-
-        <header>
-
-        </header>
-        
-        <nav>
-           
-        </nav>
-
-        <main>
-
-            <h1> Fritz Fevrier </h1>
-        </main>
-      
-        <footer>
-           
-        </footer>
-    </div>
-</body>
-
-</html>
-
+    
