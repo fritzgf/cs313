6278d5aee43d0388afa5257e50b5fac3a1de5728
Fritz Fevrier <fev17001@byui.edu>
2018-05-08 16:42:24 -0600

    messages

--- web/view/result.php
+++ web/view/result.php
@@ -4,14 +4,16 @@
 <head> </head>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-Your email address is: <?php echo $_POST["email"]; ?>
 <?php
-if(isset($_POST['submit'])){
-$selected_val = $_POST['major'];  // Storing Selected Value In Variable
-echo "You have selected :" .$selected_val;  // Displaying Selected Value
-}
+echo "<h1>Result </h>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $major;
+echo "<br>";
+echo $comment;
+
 ?>
 </body>
 </html>
