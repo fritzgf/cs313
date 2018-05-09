ff8eebd6e22a9580009c949fec85fe65fbb63986
Fritz Fevrier <fev17001@byui.edu>
2018-05-08 18:11:29 -0600

    new changes

--- web/view/result.php
+++ web/view/result.php
@@ -6,13 +6,13 @@
 
 <?php
 // define variables and set to empty values
-$name = $email = $gender = $comment = $website = "";
+$name = $email = $major = $comment = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = test_input($_POST["name"]);
   $email = test_input($_POST["email"]);
-  $comment = test_input($_POST["comment"]);
-  $gender = test_input($_POST["gender"]);
+  $comment = test_input($_POST["major"]);
+  $gender = test_input($_POST["comment"]);
 }
 
 function test_input($data) {
