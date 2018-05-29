7c144947e8c60ebe8b0f87c624470d1b0a2f7371
Fritz Fevrier <fev17001@byui.edu>
2018-05-29 13:22:02 -0600

    save changes

--- web/library/dbacess.php
+++ web/library/dbacess.php
@@ -29,6 +29,7 @@ die();
 $user_iq = $_GET["interviewText"];
 
 $query= "SELECT iq.interviewText, iq.date, u.user_id, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id";
+var_dump($query);
 $stmt = $db->prepare($query);
 var_dump($stmt);
 $stmt->bindValue(':interviewText', $user_iq, PDO::PARAM_STR);
