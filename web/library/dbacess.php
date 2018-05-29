03e8389705e4d229521d9fffe18fe720dd7ac09c
Fritz Fevrier <fev17001@byui.edu>
2018-05-29 13:25:05 -0600

    save changes

--- web/library/dbacess.php
+++ web/library/dbacess.php
@@ -31,9 +31,10 @@ $user_iq = $_GET["interviewText"];
 $query= "SELECT iq.interviewText, iq.date, u.user_id, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id";
 var_dump($query);
 $stmt = $db->prepare($query);
-var_dump($stmt);
 $stmt->bindValue(':interviewText', $user_iq, PDO::PARAM_STR);
 $stmt->execute();
+exit;
+var_dump($stmt);
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($statement->fetchAll(PDO::FETCH_ASSOC) as $rows)
