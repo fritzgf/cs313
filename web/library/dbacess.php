80cfe2f824c498f83c50df8185314d3f1de01cf1
Fritz Fevrier <fev17001@byui.edu>
2018-05-29 14:03:51 -0600

    save changes

--- web/library/dbacess.php
+++ web/library/dbacess.php
@@ -31,7 +31,7 @@ die();
 
 foreach ($db->query('SELECT iq.interviewText, iq.date, iq.user_id, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id') as $row)
 {
-  var_dump($row);
+ 
   echo  $row['interviewText'];
   echo $row['date'];
   echo $row['firstName'];
