8a753ef389eaa6d19cc453922f2787a7cadf7f66
Fritz Fevrier <fev17001@byui.edu>
2018-05-29 13:46:19 -0600

    save changes

--- web/library/dbacess.php
+++ web/library/dbacess.php
@@ -25,14 +25,11 @@ echo 'Error!:' .$ex->getMessage();
 die();
 
 }
-$query= "SELECT iq.interviewText, iq.date, u.user_id, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id";
-
-foreach ($db->query('SELECT * FROM scriptures') as $row)
+foreach ($db->query('SELECT iq.interviewText, iq.date, u.user_id, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id') as $row)
 {
-     
-    echo "$interviewText";
-    echo "<br>";
-    echo "$date";
-  
+  echo  $row["interviewText"];
+  echo $row["date"];
+  echo $row["firstName"];
+    echo "<br>";  
 }
-  ?>
+?>
