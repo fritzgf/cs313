39efaf268bf112fa55ad7115b624878318feaa7b
Fritz Fevrier <fev17001@byui.edu>
2018-05-29 13:52:27 -0600

    save changes

--- web/library/dbacess.php
+++ web/library/dbacess.php
@@ -12,6 +12,7 @@ $dbPort = $dbopts["port"];
 $dbUser = $dbopts["user"];
 $dbPassword = $dbopts["pass"];
 $dbName = ltrim($dbopts["path"],'/');
+echo "about to create the connection";
 
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
@@ -25,8 +26,12 @@ echo 'Error!:' .$ex->getMessage();
 die();
 
 }
+
+echo "right before the loop";
+
 foreach ($db->query('SELECT iq.interviewText, iq.date, u.user_id, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id') as $row)
 {
+  var_dump($row);
   echo  $row['interviewText'];
   echo $row['date'];
   echo $row['firstName'];
