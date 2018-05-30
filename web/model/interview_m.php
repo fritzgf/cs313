be6afc316c0ed26d74b5e21aa8547a6357f63785
Fritz Fevrier <fev17001@byui.edu>
2018-05-29 22:27:45 -0600

    new changes

--- web/model/interview_m.php
+++ web/model/interview_m.php
@@ -4,13 +4,13 @@
 <?php
 foreach ($db->query('SELECT iq.interviewText, iq.date, iq.user_id, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id') as $row)
 {
- 
+  echo "<hr>";  
   echo  $row['interviewtext'];
   echo "<br>"; 
   echo $row['date'];
   echo "<hr>";  
   echo "<br>";  
   echo $row['firstName'];
-    echo "<hr>";  
+   
 }
 ?>
