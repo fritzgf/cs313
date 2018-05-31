2235d68cf83f85ab9ae1319114a7488175824163
Fritz Fevrier <fev17001@byui.edu>
2018-05-30 20:28:37 -0600

    new changes

--- web/common/nav.php
+++ web/common/nav.php
@@ -2,6 +2,6 @@
                 <li><a href="." title="Acme">Home</a></li>
                 <li><a href="#" title="interview questions">Interview Question</a> </li>
                 <li><a href="#" title="Ask questions">Ask questions</a></li>
-                <li><a href="#" title="Articles"></a> Articles</li>
+                <li><a href="#" title="Articles">Articles </a> </li>
                 <li><a href="#" title="login">Login</a></li>
             </ul>
