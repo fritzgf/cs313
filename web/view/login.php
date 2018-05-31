9e6e7117587045142484ec7ea8388912c2346b0c
Fritz Fevrier <fev17001@byui.edu>
2018-05-30 21:10:34 -0600

    new changes

--- web/view/login.php
+++ web/view/login.php
@@ -7,6 +7,63 @@
     <title>Login</title>
 </head>
 <body>
-    
+<div class="wrapper">
+
+<header class="page-header">
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/header.php'; ?>
+
+
+
+
+</header>
+
+<nav id="page-nav">
+
+    <?php echo $navList; ?>
+</nav>
+
+<main class="page-main">
+
+    <section id="home-content">
+        
+        <form class="logForm" method="post" action="interviewp.php">
+
+
+            <div class="login-page">
+
+                <h1 id="signIn">Sign in</h1>
+                <input  type="email" name="clientEmail" id="clientEmail" required placeholder="Enter a valid email address" <?php
+                if (isset($clientEmail)) {
+                    echo "value='$clientEmail'";
+                }
+                ?>>
+                
+
+                <input name= "clientPassword" type="password" placeholder="Enter Password" id="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"> 
+
+              
+                <input type="submit" name="submit" id="logbtn" value="Sign in"> 
+                <input type="hidden" name="action" value="login">
+                <span class="psw">Forgot password? <a href="#">send reset email</a></span>
+
+                <p>  <a class="create-account" href="./../view/register.php">Create an account </a></p>
+
+            </div>
+
+        </form>
+
+
+    </section>
+
+
+
+</main>
+
+<footer id="page-footer">
+    <hr>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/footer.php'; ?>
+</footer>
+</div>
 </body>
 </html>
--- web/view/register.php
+++ web/view/register.php
@@ -7,6 +7,71 @@
     <title>Register</title>
 </head>
 <body>
-    
+<div class="wrapper">
+
+<header class="page-header">
+
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/header.php'; ?>
+
+
+
+</header>
+
+<nav id="page-nav">
+
+    <?php echo $navList; ?>
+</nav>
+
+
+<main class="page-main">
+
+    <section id="home-content">
+
+        <?php
+        if (isset($message)) {
+            echo $message;
+        }
+        ?>
+
+
+        <form method="post" action="login.php" class="form">
+
+            <div class="register-page"> 
+                <h1>Sign up</h1>
+
+                <label for="clientFirstname" > First Name </label> 
+                <input type="text" placeholder="Joe" name="clientFirstname" id="clientFirstname" <?php if (isset($clientFirstname)) { echo "value='$clientFirstname'"; }?> required>
+                
+
+                <label for="clientLastname" > Last Name </label>
+                <input  type="text"  placeholder="John" name="clientLastname"  id="clientLastname" <?php if (isset($clientLastname)) { echo "value='$clientLastname'"; }?> required> 
+                
+
+                <label for="clientEmail"> Email address </label>
+                <input  type="email" name="clientEmail" id="clientEmail" required placeholder="Enter a valid email address" <?php if (isset($clientEmail)) { echo "value='$clientEmail'"; }?>>
+           
+
+                <label for="clientPassword" > Password </label>
+                <input type="password"  name="clientPassword" id="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$">
+       
+                <br>
+                <input type="submit" name="submit" id="regbtn" value="Register">
+                <!-- Add the action name - value pair -->
+                <input type="hidden" name="action" value="register">
+
+            </div>  
+
+        </form> 
+
+    </section>
+
+</main>
+
+<footer id="page-footer">
+    <hr>
+    <?php ?>
+</footer>
+</div>
 </body>
 </html>
