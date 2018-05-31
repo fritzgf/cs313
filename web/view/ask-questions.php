caf439cabeed015829b8cc50ea21668f388d8643
Fritz Fevrier <fev17001@byui.edu>
2018-05-30 22:58:44 -0600

    new changes

--- web/view/ask-questions.php
+++ web/view/ask-questions.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./../css/stylesheet.css" media="screen">
     <title>Ask Questions</title>
 </head>
 <body>
@@ -22,9 +23,7 @@
 
 <main class="page-main">
 <section id="home-content">
-<?php
-require("./../model/interviewq-model.php");  
-?>
+
 <form action="interviewq.php" method="post">
 
 <label> Ask questions </label> <br>
