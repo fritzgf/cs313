ee8fed052f4250903da1fee1b7af3b747ee3716a
Fritz Fevrier <fev17001@byui.edu>
2018-05-31 23:15:08 -0600

    add new files

--- web/accounts/insertReg.php
+++ web/accounts/insertReg.php
@@ -21,13 +21,14 @@ $email = htmlspecialchars($_POST["email"]);
 // $db = get_db();
 
 $query = "INSERT INTO users (firtsName, lastName, email) VALUES (:firstName, :lastName, :email)";
-var_dump($query); 
-exit;
+
 $statement = $db->prepare($query);
 $statement->bindValue(":firstName", $firtstName, PDO::PARAM_STR);
 $statement->bindValue(":lastName", $lastName, PDO::PARAM_STR);
 $statement->bindValue(":email", $email, PDO::PARAM_STR);
 $statement->execute();
+var_dump($statement); 
+exit;
 // header("Location: courseDetails.php?course_id=$courseId");
 die();
 ?>
