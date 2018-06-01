37de4d42f58fa460a9bc104f87eaac4ea7299b19
Fritz Fevrier <fev17001@byui.edu>
2018-06-01 01:00:10 -0600

    add new files

--- web/model/interviewq-model.php
+++ web/model/interviewq-model.php
@@ -3,27 +3,28 @@
 $db = get_db();
 
 
-// foreach ($db->query('SELECT iq.interviewText, iq.date, iq.user_id, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id') as $row)
-// {
-//   echo "<hr>";  
-//   echo  $row['interviewtext'];
-//   echo "<br>"; 
-//   echo $row['date'];
-//   echo "<hr>"; 
-//   echo "<br>";   
-// }
-$query = "SELECT iq.interviewText, iq.date, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id";
-$statement = $db->prepare($query);
-// Bind any variables I need, here...
-$statement->execute();
-$interview_questions = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($interview_questions as $interview_question) {
-	$id = $interview_question["id"];
-	$interviewText = $interview_question["interviewText"];
-   $firstName = $interview_question["firstName"];
-   $date = $interview_question["date"];
-	echo "<li><a href='courseDetails.php?course_id=$id'>$interviewText - $firstName -  $date</a></li>";
+foreach ($db->query('SELECT iq.interviewText, iq.date, iq.user_id, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id') as $row)
+{
+  echo "<hr>";  
+  echo  $row['interviewtext'];
+  echo "<br>"; 
+  echo $row['date'];
+  echo "<hr>"; 
+  echo "<br>";   
 }
+// $query = "SELECT iq.interviewText, iq.date, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id";
+// $statement = $db->prepare($query);
+// // Bind any variables I need, here...
+// $statement->execute();
+// $interview_questions = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-?>
+// foreach ($interview_questions as $interview_question) {
+// 	$id = $interview_question["id"];
+// 	$interviewText = $interview_question["interviewText"];
+//    $firstName = $interview_question["firstName"];
+//    $date = $interview_question["date"];
+   
+// 	echo "<li><a href='courseDetails.php?course_id=$id'>$interviewText - $firstName -  $date</a></li>";
+// }
+
+// ?>
