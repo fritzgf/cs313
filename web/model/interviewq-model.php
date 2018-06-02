41c0775e2752ede9c4f8502cba9bbda625db3df3
Fritz Fevrier <fev17001@byui.edu>
2018-06-02 12:43:08 -0600

    add new files

--- web/model/interviewq-model.php
+++ web/model/interviewq-model.php
@@ -3,31 +3,33 @@
 $db = get_db();
 
 
-// foreach ($db->query('SELECT iq.interviewText, iq.date, iq.user_id, u.firstName FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id') as $row)
-// {
-//   echo "<hr>";  
-//   echo  $row['interviewtext'];
-//   echo "<br>"; 
-//   echo $row['date'];
-//   echo "<hr>"; 
-//   echo "<br>";   
-// }
-$query = "SELECT id, interviewText, date FROM interview_questions";
-$statement = $db->prepare($query);
-// Bind any variables I need, here...
-$statement->execute();
-$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($db->query('SELECT iq.interviewtext, iq.date, iq.user_id, u.firstname FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id') as $row)
+{
+  echo "<hr>";  
+  echo  $row['interviewtext'];
+  echo "<hr>";  
+  echo  $row['firstname'];
+  echo "<br>"; 
+  echo $row['date'];
+  echo "<hr>"; 
+  echo "<br>";   
+}
+// $query = "SELECT id, interviewText, date FROM interview_questions";
+// $statement = $db->prepare($query);
+// // Bind any variables I need, here...
+// $statement->execute();
+// $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($rows as $row) {
-	$id = $row["id"];
-	$interviewText = $row["interviewText"];
-   $firstName = $row["firstName"];
-   $date = $row["date"];
+// foreach ($rows as $row) {
+// 	$id = $row["id"];
+// 	$interviewText = $row["interviewText"];
+//    $firstName = $row["firstName"];
+//    $date = $row["date"];
    
 
-  echo "<li><a href='courseDetails.php?user_id=$id'>$interviewText </a></li>";
-  echo "<li><a href='courseDetails.php?user_id=$id'> $firstName </a></li>";
-  echo "<li><a href='courseDetails.php?user_id=$id'> $date</a></li>";
-}
+//   echo "<li><a href='interviewq.php?user_id=$id'>$interviewText </a></li>";
+//   echo "<li><a href='interviewq.php?user_id=$id'> $firstName </a></li>";
+//   echo "<li><a href='interviewq.php?user_id=$id'> $date</a></li>";
+// }
 
 ?>
