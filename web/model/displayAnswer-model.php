2c1b2656c4f2ca9e27f8e14ccd651bd100d0377a
Fritz Fevrier <fev17001@byui.edu>
2018-06-02 21:02:19 -0600

    add new files

--- web/model/displayAnswer-model.php
+++ web/model/displayAnswer-model.php
@@ -1,6 +1,6 @@
 <?php
 require("./../library/connections.php");
-$linksId = htmlspecialchars($_GET["user_id"]);
+$linksId = htmlspecialchars($_GET["interview_id"]);
 $db = get_db();
 $query = "SELECT iq.interviewtext, iq.date, iq.user_id, u.firstname FROM interview_questions as iq INNER JOIN users as u ON iq.user_id= u.id WHERE id=:id";
 $statement = $db->prepare($query);
