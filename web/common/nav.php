63b07fe769f924a709ced9fd47665da756c2635f
Fritz Fevrier <fev17001@byui.edu>
2018-06-03 23:48:54 -0600

    testing ansers

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,7 +1,7 @@
 <ul class="flexlist">
                 <li><a href="home.php" title="Acme">Home</a></li>
                 <li><a href="interviewq.php" title="interview questions">Interview Question</a> </li>
-                <li><a href="jobdevelopers.php" title="job developers">Job Developers</a></li>
+                <!-- <li><a href="jobdevelopers.php" title="job developers">Job Developers</a></li> -->
                 <li><a href="login.php" title="Login">Login </a> </li>
            
             </ul>
