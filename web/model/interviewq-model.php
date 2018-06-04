fd0810b8ee1697484940b8464c48b7bf832df8d3
Fritz Fevrier <fev17001@byui.edu>
2018-06-03 23:23:32 -0600

    testing ansers

--- web/model/interviewq-model.php
+++ web/model/interviewq-model.php
@@ -26,7 +26,7 @@ foreach ($rows as $row) {
    $date = $row["date"];
    
    echo "<hr>";
-  echo "<li><a href='interviewq.php?interview_id=$id'>$interviewtext <br> $firstname - $date </a></li>";
+   echo "<li><a href='interviewq.php?interview_id=$id'>$interviewtext <br> $firstname - $date </a></li>";
   echo "<hr>";
   // echo "<li><a href='interviewq.php?user_id=$id'> $firstname </a></li>";
   // echo "<li><a href='interviewq.php?user_id=$id'> $date</a></li>";
