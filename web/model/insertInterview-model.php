d06cba0f0588429d254502b1d6bebe60b201b8c0
Fritz Fevrier <fev17001@byui.edu>
2018-06-05 21:05:22 -0600

    save changes

--- web/model/insertInterview-model.php
+++ web/model/insertInterview-model.php
@@ -14,9 +14,9 @@ $statement = $db->prepare($query);
 
 $statement->bindValue(":interviewtext", $interviewtext, PDO::PARAM_STR);
 $statement->bindValue(":id", $links, PDO::PARAM_INT);
-
-$statement->execute();
 echo" 1\n";
+$statement->execute();
+
 header("Location: ../view/answers.php?interview_id=$links");
 die();
 ?>
