b738c661199fe958c65ef093143ea62eae399120
Fritz Fevrier <fev17001@byui.edu>
2018-06-05 21:03:39 -0600

    save changes

--- web/model/insertInterview-model.php
+++ web/model/insertInterview-model.php
@@ -3,19 +3,20 @@ require("./../library/connections.php");
 $db = get_db();
 
 $links = htmlspecialchars($_POST["id"]);
-$date = htmlspecialchars($_POST["date"]);
+
 $interviewtext = htmlspecialchars($_POST["interviewtext"]);
 // echo "interview_id: $links\n";
 // echo "date: $date\n";
 // echo "interviewtext: $interviewtext\n";
 
-$query = "INSERT INTO interview_questions (interviewtext, id, date) VALUES (:interviewtext, :id, :date)";
+$query = "INSERT INTO interview_questions (interviewtext, id) VALUES (:interviewtext, :id)";
 $statement = $db->prepare($query);
-var_dump($query);
+
 $statement->bindValue(":interviewtext", $interviewtext, PDO::PARAM_STR);
-$statement->bindValue(":interview_id", $links, PDO::PARAM_INT);
-$statement->bindValue(":date", $date, PDO::PARAM_STR);
+$statement->bindValue(":id", $links, PDO::PARAM_INT);
+
 $statement->execute();
+echo" 1\n";
 header("Location: ../view/answers.php?interview_id=$links");
 die();
 ?>
