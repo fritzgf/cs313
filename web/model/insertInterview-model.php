39ddfbdb046efc104e3bfc21d9cf57416c99c1c2
Fritz Fevrier <fev17001@byui.edu>
2018-06-12 01:52:30 -0600

    test

--- web/model/insertInterview-model.php
+++ web/model/insertInterview-model.php
@@ -7,15 +7,16 @@ $interviewtext = htmlspecialchars($_POST["interviewtext"]);
 // echo "interview_id: $links\n";
 // echo "date: $date\n";
 // echo "interviewtext: $interviewtext\n";
-
+echo" 1\n";
 $query = "INSERT INTO interview_questions (interviewtext) VALUES (:interviewtext)";
+echo" 1\n";
 $statement = $db->prepare($query);
-
+echo" 1\n";
 $statement->bindValue(":interviewtext", $interviewtext, PDO::PARAM_STR);
 
-
+echo" 1\n";
 $statement->execute();
-// echo" 1\n";
+echo" 1\n";
 header("Location: ../view/answers.php");
 die();
 ?>
