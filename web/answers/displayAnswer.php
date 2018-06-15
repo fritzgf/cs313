d2e9c91bb9c7e13fc87e1bb442b8f012932aaa5c
Fritz Fevrier <fev17001@byui.edu>
2018-06-15 00:00:23 -0600

    test

--- web/answers/displayAnswer.php
+++ web/answers/displayAnswer.php
@@ -10,10 +10,10 @@ require("./../library/connections.php");
 $db = get_db();
 
 $userId= $_SESSION ["userData"]["id"];
-$interview_id= $_GET["interview_id"];
+$interviewtext= $_GET["interview_id"];
 
 
-$query = "SELECT iq.id, interview_id, iq.answer, iq.date, iq.user_id, u.firstname FROM answers as iq INNER JOIN users as u ON iq.user_id= u.id";
+$query = "SELECT iq.id, interviewtext, iq.answer, iq.date, iq.user_id, u.firstname FROM answers as iq INNER JOIN users as u ON iq.user_id= u.id";
 $statement = $db->prepare($query);
 // Bind any variables I need, here...
 $statement->execute();
@@ -24,7 +24,7 @@ foreach ($rows as $row) {
 	$answer = $row["answer"];
    $firstname = $row["firstname"];
    $date = $row["date"]; 
-   $interview_id = $row["interview_id"]; 
+   $interviewtext = $row["interviewtext"]; 
    
    echo "<hr>";
    echo "<li><a href='answers.php?interview_id=$id'>$interviewtext <br> $firstname - $date </a></li>";
