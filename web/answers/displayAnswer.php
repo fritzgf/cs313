e2ca529dfa1a3220312cd6d149c8afd4c84531d2
Fritz Fevrier <fev17001@byui.edu>
2018-06-14 23:53:12 -0600

    test

--- web/answers/displayAnswer.php
+++ web/answers/displayAnswer.php
@@ -11,9 +11,7 @@ $db = get_db();
 
 $userId= $_SESSION ["userData"]["id"];
 $interview_id= $_GET["interview_id"];
-echo '$_SESSION["userData"]["id"]: $userId\n';
-echo "interview_id: $interview_id\n";
-exit;
+
 
 $query = "SELECT iq.id, interview_id, iq.answer, iq.date, iq.user_id, u.firstname FROM answers as iq INNER JOIN users as u ON iq.user_id= u.id";
 $statement = $db->prepare($query);
