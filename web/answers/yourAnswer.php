8084af00ffc51d101787f606660feeea96e90777
Fritz Fevrier <fev17001@byui.edu>
2018-06-14 22:25:28 -0600

    test

--- web/answers/yourAnswer.php
+++ web/answers/yourAnswer.php
@@ -28,14 +28,14 @@ $statement->bindValue(":answer", $answer, PDO::PARAM_STR);
 $statement->bindValue(":user_id", $userId, PDO::PARAM_INT);
 $statement->bindValue(":interview_id", $interviewId, PDO::PARAM_INT);
 $statement->bindValue(":date", $date, PDO::PARAM_STR);
-// try{
+try{
 $statement->execute();
-// } catch(PDOException $e){ 
-// echo $e->getMessage();
-// exit;
-// }
-var_dump($statement);
+} catch(PDOException $e){ 
+echo $e->getMessage();
 exit;
+}
+// var_dump($statement);
+// exit;
 // header("Location: ../view/interviewq.php");
 die();
 
