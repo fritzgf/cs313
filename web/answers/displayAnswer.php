1584dc8c16fcd6c817d0680edd53265977e07e7f
Fritz Fevrier <fev17001@byui.edu>
2018-06-18 18:21:36 -0600

    save changes

--- web/answers/displayAnswer.php
+++ web/answers/displayAnswer.php
@@ -5,32 +5,52 @@ require("./../library/connections.php");
 
 $db = get_db();
 
+
 $userId= $_SESSION ["userData"]["id"];
-$linksId =($_GET["interview_id"]);
-// echo '$_SESSION["userData"]["id"]: $userId\n';
-// echo "interview_id: $interview_id\n";
-// exit;
-if($linksId==NULL){
-  $linksId = htmlspecialchars($_POST["interview_id"]);
-  
-}
-$query = "SELECT iq.id, iq.interviewtext, iq.answer, iq.date, iq.user_id, u.firstname FROM answers as iq INNER JOIN users as u ON iq.user_id= u.id";
+$interview_id= $_GET["interview_id"];
+$query = "SELECT iq.id, iq.answer, iq.date, iq.user_id, u.firstname FROM answers as iq INNER JOIN users as u ON iq.user_id= u.id";
 $statement = $db->prepare($query);
 // Bind any variables I need, here...
 $statement->execute();
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-
 foreach ($rows as $row) {
-	$userId = $row["id"]; 
+	$userId = $row["user_id"]; 
 	$answer = $row["answer"];
    $firstname = $row["firstname"];
    $date = $row["date"]; 
-   $interviewtext = $row["interviewtext"]; 
    
    echo "<hr>";
-   echo "$interviewtext <br> $firstname - $date </li>";
-  echo "<li>$answer <br> $firstname - $date </li>";
+  echo "<li><a href='answers.php?answer_id=$user_id'>$answer <br> $firstname - $date </a></li>";
   echo "<hr>";
-
 }
 ?>
+
+// $userId= $_SESSION ["userData"]["id"];
+// $linksId =($_GET["interview_id"]);
+// // echo '$_SESSION["userData"]["id"]: $userId\n';
+// // echo "interview_id: $interview_id\n";
+// // exit;
+// if($linksId==NULL){
+//   $linksId = htmlspecialchars($_POST["interview_id"]);
+
+// }
+// $query = "SELECT iq.id, iq.interviewtext, iq.answer, iq.date, iq.user_id, u.firstname FROM answers as iq INNER JOIN users as u ON iq.user_id= u.id";
+// $statement = $db->prepare($query);
+// // Bind any variables I need, here...
+// $statement->execute();
+// $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// foreach ($rows as $row) {
+// 	$userId = $row["id"]; 
+// 	$answer = $row["answer"];
+//    $firstname = $row["firstname"];
+//    $date = $row["date"]; 
+//    $interviewtext = $row["interviewtext"]; 
+   
+//    echo "<hr>";
+//    echo "$interviewtext <br> $firstname - $date </li>";
+//   echo "<li>$answer <br> $firstname - $date </li>";
+//   echo "<hr>";
+
+// }
+// ?>
