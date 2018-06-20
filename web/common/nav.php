29afedb3349ab962272dca1d81769fc2ea62e36d
Fritz Fevrier <fev17001@byui.edu>
2018-06-20 00:54:07 -0600

    save changes

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,12 +1,12 @@
-<?php
-session_start();
-
-?>
 <ul class="flexlist">
                 <li><a href="home.php" title="Acme">Home</a></li>
                 <li><a href="interviewq.php" title="interview questions">Interview Question</a> </li>
                 <!-- <li><a href="jobdevelopers.php" title="job developers">Job Developers</a></li> -->
-                <li>  <?php
+                <!-- <li><a href="login.php" title="Login">Login </a> </li> -->
+           
+            </ul>
+
+             <?php
 
 if(isset($_SESSION["userData"]) == TRUE){
     include '../view/logout.php';
@@ -15,6 +15,3 @@ if(isset($_SESSION["userData"]) == TRUE){
     include '../view/login.php';
 }
 ?>
-</li>
-           
-            </ul>
