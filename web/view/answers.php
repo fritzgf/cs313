6349c7de1985924e4b0dbaedd4625cc6650a09a6
Fritz Fevrier <fev17001@byui.edu>
2018-06-20 03:03:56 -0600

    save changes

--- web/view/answers.php
+++ web/view/answers.php
@@ -43,7 +43,7 @@ require("./../model/displayAnswer-model.php");
 <div class="login-page">
 <label> Share your Answer </label> <br>
 <input value="2018-06-14" name="date" type="input" class="form-control" placeholder="YYYY-MM-DD"/><br> <br>
-<textarea name="answer" placeholder="Type your answer" rows="35" cols="40">  </textarea> <br>
+<textarea name="answer" placeholder="Type your answer" rows="20" cols="25">  </textarea> <br>
 <input type="hidden" name="interview_id" value="<?php echo $linksId; ?>">
 <input type="hidden" name="user_id" value="<?php echo $_SESSION["userData"]["id"]; ?>">
 
