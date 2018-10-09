f249bb2164c7cac7d0bca3f8cf65a4c7297be36e
Fritz Fevrier <fev17001@byui.edu>
2018-10-09 03:49:42 -0600

    add new files

--- web/shoppingcart/viewCart.php
+++ web/shoppingcart/viewCart.php
@@ -3,11 +3,6 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -46,7 +41,7 @@ and open the template in the editor.
                     foreach ($_SESSION['price'] as $price) {
                         $total += $price;
                     }
-                    echo "</p>Total: <span id='orderTotal'>" . $total . "</span> Gold Pieces";
+                    echo "</p>Total: <span id='orderTotal'>" . $total . "</span> Dollars";
                     ?>
                 </section>
                 
