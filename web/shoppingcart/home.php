f5ada51f6b75d6da68b5850299f105cca35429d8
Fritz Fevrier <fev17001@byui.edu>
2018-10-09 03:18:02 -0600

    add new files

--- web/shoppingcart/home.php
+++ web/shoppingcart/home.php
@@ -20,21 +20,17 @@ if(!isset($_SESSION['cart'])) {
 <body>
     <div id="wrapper">
     <header class="page-header">
-
-     <a href="viewCart.php">
-    <img src="images/ShoppingCartLogo.png" alt="Shopping Cart logo" title=" logo for MyStore site"> </a>
-            
-           
         </header>
 
            <nav id="page-nav">
             <ul>
                 <li><a id= "home_color" href="home.php" title="Go to home page">Home</a></li>
-                <li><a id="eufy" href="eufy.html" title="Acme's product"></a></li>
-                <li><a id="huawei" href="huawei.html" title="Acme's product"></a></li>
-                <li><a id="china" href="china.html" title="Acme's product"></a></li>
-                <li><a id="august" href="august.html" title="Acme's product"></a></li>
-               
+                <li><a id="eufy" href="eufy.html" title=""></a></li>
+                <li><a id="huawei" href="huawei.html" title=""></a></li>
+                <li><a id="china" href="china.html" title=""></a></li>
+                <li><a id="august" href="august.html" title=" "></a></li>
+               <li>  <a href="viewCart.php">
+    <img src="images/ShoppingCartLogo.png" alt="Shopping Cart logo" title=" logo for MyStore site"> </a> </li>
             </ul>
 
         </nav>
@@ -43,19 +39,6 @@ if(!isset($_SESSION['cart'])) {
        <main class="page-main">
        <section id="home-content">
                 
-                <!-- <div class="description-item">
-                <img id="pic" src="images/site/feature.jpg" alt="rocket acme strapped">
-
-<ul id="rocket-item">
-    <li>
-        <h2>Acme Rocket</h2>
-    </li>
-    <li>Quick lighting fuse</li>
-    <li>NHTSA approved seat belts</li>
-    <li>Mobile launch stand included</li>
-    <li><a href="#"><img id="actionbtn" alt="Add to cart button" src="/acme/images/site/iwantit.gif"></a></li>
-</ul>
-</div> -->
 <section id="recipe-content">
 
 <div id="recipe-images">
@@ -63,28 +46,14 @@ if(!isset($_SESSION['cart'])) {
    
     <div id="image-wrapper">
     
-                
-         <!-- <div id="item-details">
-                    
-                </div>
-                
-                <table>
-                    <tr> <figure> -->
+            
                     <figure class="featured-image">
                     <img src='images/longImage.png' alt='Myth Black and White'>
             <figcaption><button onclick="addToCart('anvil', 50)">Add to Cart</button>
                        <div  onmouseenter="itemDetails(0)" onmouseout="removeDetails()">Potion of Healing</div>
                         <p>50 Gold Pieces per vial</p></figcaption>
                   </figure>
-                        <!-- <td><button onclick="addToCart('anvil', 50)">Add to Cart</button></td>
-                        <td onmouseenter="itemDetails(0)" onmouseout="removeDetails()">Potion of Healing</td>
-                        <td>50 Gold Pieces per vial</td>
-                    </tr> -->
-                    <!-- <tr>
-                        <button onclick="addToCart('bomb', 90)">Add to Cart</button></td>
-                        <td onmouseenter="itemDetails(1)" onmouseout="removeDetails()">Philter of Love</td>
-                        <td>90 Gold Pieces per vial</td>
-                    </tr> -->
+                       
                     <figure>
                     <img src='images/Huawei.png' alt='Myth Black and White'>
 
@@ -95,11 +64,7 @@ if(!isset($_SESSION['cart'])) {
                         <p>90 Gold Pieces per vial</p>
                                 </figcaption>
                                 </figure>
-                    <!-- <tr>
-                        <td><button onclick="addToCart('catapult', 120)">Add to Cart</button></td>
-                        <td onmouseenter="itemDetails(2)" onmouseout="removeDetails()">Dust of Dryness</td>
-                        <td>120 Gold Pieces per pinch</td>
-                    </tr> -->
+
 
                      <figure>
                      <img src='images/zoloImage.png' alt='Myth Black and White'>
@@ -110,11 +75,7 @@ if(!isset($_SESSION['cart'])) {
                         <p>120 Gold Pieces per pinch</p>
                                 </figcaption>
                             </figure>
-                    <!-- <tr>
-                        <td><button onclick="addToCart('helmet', 120)">Add to Cart</button></td>
-                        <td onmouseenter="itemDetails(3)" onmouseout="removeDetails()">Elixir of Health</td>
-                        <td>120 Gold Pieces per vial</td>
-                    </tr> -->
+        
                    
                     <figure>
                     <img src='images/augustVenus.png' alt='Myth Black and White'>
@@ -126,11 +87,7 @@ if(!isset($_SESSION['cart'])) {
                         <p>200 Gold Pieces per box</p>
                                 </figcaption>
                             </figure>
-                    <!-- <tr>
-                        <td><button onclick="addToCart('hole', 200)">Add to Cart</button></td>
-                        <td onmouseenter="itemDetails(4)" onmouseout="removeDetails()">Marvelous Pigments</td>
-                        <td>200 Gold Pieces per box</td>
-                    </tr> -->
+        
                     <figure>
                     <img src='images/boxImage.png' alt='Myth Black and White'>
 
@@ -140,12 +97,6 @@ if(!isset($_SESSION['cart'])) {
                         <p>200 Gold Pieces per box</p>
                                 </figcaption>
                             </figure>
-                    <!-- <tr>
-                        <td><button onclick="addToCart('piano', 400)">Add to Cart</button></td>
-                        <td onmouseenter="itemDetails(5)" onmouseout="removeDetails()">Sovereign Glue</td>
-                        <td>400 Gold Pieces per vial</td>
-                    </tr>
-                </table> -->
                 
                 <figure>
                 <img src='images/eufy.png' alt='Myth Black and White'>
@@ -160,17 +111,9 @@ if(!isset($_SESSION['cart'])) {
 
                 </div>
                     </div>
-                    <!-- <div id="product-review">
+                    
 
-<h3>Acme Rocket Reviews</h3>
-<ul>
-    <li>"I don't know how I ever caught roadrunners before this." (9/10)</li>
-    <li>"That thing was fast!" (4/5)</li>
-    <li>"Talk about fast delivery." (5/5)</li>
-    <li>"I didn't even have to pull the meat apart." (4.5/5)</li>
-    <li>"I'm on my thirtieth one. I love these things!" (5/5)</li>
-</ul>
-</div> -->
+
 
 </section>
 
@@ -199,7 +142,7 @@ if(!isset($_SESSION['cart'])) {
        
        <footer id="page-footer">
             <hr>
-            <p> &copy; 2017 ACME Inc., All rights reserved. <br> All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.</p>
+            <p> &copy; 2018 MyStore Inc., All rights reserved.</p>
         </footer>
     </div>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
