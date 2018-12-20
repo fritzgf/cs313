32d5f83569959a77a896de964312ea05b69d4093
Fritz Fevrier <fev17001@byui.edu>
2018-12-19 17:21:04 -0700

    add files

---
+++ web/cit265project/allObjectives/CSS3Transforms/example.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="img/favicon.png">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Clocks</title>
+</head>
+
+<body>
+
+<div class="clock">
+    <div class="clock-face">
+        <div class='number' id='number-12'>0</div>
+        <div class='number' id='number-3'>III</div>
+        <div class='number' id='number-6'>VI</div>
+        <div class='number' id='number-9'>IX</div>
+
+        <div class="hand hour-hand"></div>
+        <div class="hand min-hand"></div>
+        <div class="hand second-hand"></div>
+
+    </div>
+</div>
+
+<script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ web/cit265project/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html  lang="en">
+
+<head>
+    <!-- head describes the page to the browser-->
+
+    <meta charset="utf-8">
+    <title> Home | fritzgf.github.io</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous">
+    </script>
+
+</head>
+
+<body>
+<h1>Welcome to My Repository </h1>
+<!--body holds the content of the page-->
+<div id=" wrapper">
+
+
+    <header>
+
+    </header>
+    <!-- links to identify our site-->
+    <nav>
+        <ul>
+            <li> <a href="index.php"> Home</a></li>
+            <li> <a href="myStore.php"> Final Project</a></li>
+            <li> <a href="allObjectives/basicJavascript/example.php"> Basic JavaScript</a></li>
+            <li> <a href="allObjectives/Canvas/example.php"> Canvas</a></li>
+            <li> <a href="allObjectives/CSS3Transforms/example.php">Triggering Transformation</a> </li>
+            <li> <a href="allObjectives/cssAnimation/example.php">Triggering Animation</a></li>
+            <li> <a href="allObjectives/cssTransition/example.php">Triggering CSS3 Transition </a></li>
+            <li> <a href="allObjectives/cssTransitionJava/example.php">CSS And JavaScript</a></li>
+            <li> <a href="allObjectives/DOM-Manupulation/example.php"> DOM Manipulation Using createElement, appendChild</a></li>
+            <li> <a href="allObjectives/localStorage/example.php">LocalStorage</a></li>
+
+
+
+        </ul>
+    </nav>
+
+    <main>
+
+        <h1> Fritz Fevrier </h1>
+    </main>
+    <!--Provides information about the site-->
+    <footer>
+        &copy;2018, Fritz Fevrier
+    </footer>
+</div>
+</body>
+
+</html>
