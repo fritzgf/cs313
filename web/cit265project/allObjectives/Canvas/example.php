a73f996b3cfa69124bf4eb012f3e8030ac80c830
Fritz Fevrier <fev17001@byui.edu>
2018-12-19 17:41:47 -0700

    add files

---
+++ web/cit265project/allObjectives/Canvas/example.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="img/favicon.png">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ping Pong</title>
+</head>
+<body>
+<canvas id="gameCanvas" width="500" height="300"></canvas>
+<script src="js/script.js"></script>
+</body>
+</html>
